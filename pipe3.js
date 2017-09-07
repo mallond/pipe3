361c1e55018f5af26b514f35e7fefc2af7bcaa69
david mallon <mallond@gmail.com>
2017-09-07 16:39:13 -0500

    clean up

--- pipe3.js
+++ pipe3.js
@@ -4,6 +4,9 @@ module.exports = function (RED) {
     RED.nodes.createNode(this, config);
     var node = this;
     this.on('input', function (msg) {
+      msg.payload.total = msg.payload.total>0?msg.payload.total:msg.payload.amount;
+      msg.payload.steps.push('Pipe 3 add Shipping: '+msg.payload.shipping+' to: ' +msg.payload.total+ ' = '+msg.payload.total+msg.payload.shipping);
+      msg.payload.total = msg.payload.total+msg.payload.shipping;
       node.send(msg);
     });
   }
